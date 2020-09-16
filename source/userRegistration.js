cc9854f455064768b48ae6e0627d727343b2eae6
himanshu <himanshugharat44@gmail.com>
2020-09-16 03:53:40 -0500

    ADDED: the test and code to validate the phone number

--- source/userRegistration.js
+++ source/userRegistration.js
@@ -3,8 +3,14 @@ module.exports = {
     let regexConst = new RegExp(/^[A-Z][A-Za-z]{2,}/);
     return regexConst.test(name);
   },
-  emailValidator: function(email){
-    let regexConst = new RegExp(/^[0-9a-z]+[+_.-]?[0-9a-z]+[@][0-9a-z]+[.][a-z]{2,}[.]?[a-z]+$/);
+  emailValidator: function (email) {
+    let regexConst = new RegExp(
+      /^[0-9a-z]+[+_.-]?[0-9a-z]+[@][0-9a-z]+[.][a-z]{2,}[.]?[a-z]+$/
+    );
     return regexConst.test(email);
-  }
+  },
+  mobileNoValidator: function (number) {
+    let regexConst = new RegExp(/^[0-9]{2}[ ][0-9]{10}/);
+    return regexConst.test(number);
+  },
 };
--- test/registrationTest.js
+++ test/registrationTest.js
@@ -32,16 +32,37 @@ describe("Last Name validator ", function () {
 });
 
 describe("Email id validator ", function () {
-    it("returns true if email id is valid", function () {
-      assert.isTrue(userRegistration.emailValidator("himanshugharat3@gmail.com"));
-    });
-    it("returns false if email dont have @ symbol is not valid", function () {
-      assert.isFalse(userRegistration.emailValidator("himanshugharat.com"));
-    });
-    it("returns false if email id dont have domain name", function () {
-      assert.isFalse(userRegistration.emailValidator("himanshugharat@gmail"));
-    });
-    it("returns false if email id dont have @ and domain name", function () {
-      assert.isFalse(userRegistration.emailValidator("himanshugharat"));
-    });
+  it("returns true if email id is valid", function () {
+    assert.isTrue(userRegistration.emailValidator("himanshugharat3@gmail.com"));
   });
+  it("returns false if email dont have @ symbol is not valid", function () {
+    assert.isFalse(userRegistration.emailValidator("himanshugharat.com"));
+  });
+  it("returns false if email id dont have domain name", function () {
+    assert.isFalse(userRegistration.emailValidator("himanshugharat@gmail"));
+  });
+  it("returns false if email id dont have @ and domain name", function () {
+    assert.isFalse(userRegistration.emailValidator("himanshugharat"));
+  });
+});
+
+describe("Phone Number validator ", function () {
+  it("returns true if phone number is valid", function () {
+    assert.isTrue(userRegistration.mobileNoValidator("91 8879112321"));
+  });
+  it("returns false if phone number contains symbol ", function () {
+    assert.isFalse(userRegistration.mobileNoValidator("91@8879112321"));
+  });
+  it("returns false if phone number dont have space between code and number", function () {
+    assert.isFalse(userRegistration.mobileNoValidator("919619326117"));
+  });
+  it("returns false if phone number has letters or symbols in it", function () {
+    assert.isFalse(userRegistration.mobileNoValidator("12345556Q22ee"));
+  });
+  it("returns false if phone number is more than 10 digit", function () {
+    assert.isFalse(userRegistration.mobileNoValidator("911 887911202111"));
+  });
+  it("returns false if phone number is less than 10 digit", function () {
+    assert.isFalse(userRegistration.mobileNoValidator("91 887911"));
+  });
+});
