a39e0ea3273e8c51faf3f8d59ae8326d208e7468
himanshu <himanshugharat44@gmail.com>
2020-09-16 03:21:27 -0500

    ADDED: the test case and code for last name validation

--- test/registrationTest.js
+++ test/registrationTest.js
@@ -1,17 +1,32 @@
 const assert = require("chai").assert;
 const userRegistration = require("../source/userRegistration");
 
-describe("Name validator ", function () {
-  it("returns true if name is valid", function () {
+describe("First Name validator ", function () {
+  it("returns true if first name is valid", function () {
     assert.isTrue(userRegistration("Himanshu"));
   });
-  it("returns false if name is not valid", function () {
+  it("returns false if first name is not valid", function () {
     assert.isFalse(userRegistration("himanshu"));
   });
-  it("returns false if name starts from number", function () {
+  it("returns false if first name starts from number", function () {
     assert.isFalse(userRegistration("1Himanshu"));
   });
-  it("returns false if name starts with symbol", function () {
+  it("returns false if first name starts with symbol", function () {
     assert.isFalse(userRegistration("@Himanshu"));
+  });
+});
+
+describe("Last Name validator ", function () {
+  it("returns true if last name is valid", function () {
+    assert.isTrue(userRegistration("Gharat"));
+  });
+  it("returns false if last name is not valid", function () {
+    assert.isFalse(userRegistration("gharat"));
+  });
+  it("returns false if last name starts from number", function () {
+    assert.isFalse(userRegistration("1gharat"));
+  });
+  it("returns false if last name starts with symbol", function () {
+    assert.isFalse(userRegistration("@Gharat"));
   });
 });
