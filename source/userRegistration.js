b32ec3a17942feb7ce73ad04e0fa94bcaf7f70ed
himanshu <himanshugharat44@gmail.com>
2020-09-16 03:42:33 -0500

    ADDED: test case and code to validate the email id

--- source/userRegistration.js
+++ source/userRegistration.js
@@ -1,4 +1,10 @@
-module.exports = function (name) {
-  let regexConst = new RegExp(/^[A-Z][A-Za-z]{2,}/);
-  return regexConst.test(name);
+module.exports = {
+  nameValidator: function (name) {
+    let regexConst = new RegExp(/^[A-Z][A-Za-z]{2,}/);
+    return regexConst.test(name);
+  },
+  emailValidator: function(email){
+    let regexConst = new RegExp(/^[0-9a-z]+[+_.-]?[0-9a-z]+[@][0-9a-z]+[.][a-z]{2,}[.]?[a-z]+$/);
+    return regexConst.test(email);
+  }
 };
--- test/registrationTest.js
+++ test/registrationTest.js
@@ -3,30 +3,45 @@ const userRegistration = require("../source/userRegistration");
 
 describe("First Name validator ", function () {
   it("returns true if first name is valid", function () {
-    assert.isTrue(userRegistration("Himanshu"));
+    assert.isTrue(userRegistration.nameValidator("Himanshu"));
   });
   it("returns false if first name is not valid", function () {
-    assert.isFalse(userRegistration("himanshu"));
+    assert.isFalse(userRegistration.nameValidator("himanshu"));
   });
   it("returns false if first name starts from number", function () {
-    assert.isFalse(userRegistration("1Himanshu"));
+    assert.isFalse(userRegistration.nameValidator("1Himanshu"));
   });
   it("returns false if first name starts with symbol", function () {
-    assert.isFalse(userRegistration("@Himanshu"));
+    assert.isFalse(userRegistration.nameValidator("@Himanshu"));
   });
 });
 
 describe("Last Name validator ", function () {
   it("returns true if last name is valid", function () {
-    assert.isTrue(userRegistration("Gharat"));
+    assert.isTrue(userRegistration.nameValidator("Gharat"));
   });
   it("returns false if last name is not valid", function () {
-    assert.isFalse(userRegistration("gharat"));
+    assert.isFalse(userRegistration.nameValidator("gharat"));
   });
   it("returns false if last name starts from number", function () {
-    assert.isFalse(userRegistration("1gharat"));
+    assert.isFalse(userRegistration.nameValidator("1gharat"));
   });
   it("returns false if last name starts with symbol", function () {
-    assert.isFalse(userRegistration("@Gharat"));
+    assert.isFalse(userRegistration.nameValidator("@Gharat"));
   });
 });
+
+describe("Email id validator ", function () {
+    it("returns true if email id is valid", function () {
+      assert.isTrue(userRegistration.emailValidator("himanshugharat3@gmail.com"));
+    });
+    it("returns false if email dont have @ symbol is not valid", function () {
+      assert.isFalse(userRegistration.emailValidator("himanshugharat.com"));
+    });
+    it("returns false if email id dont have domain name", function () {
+      assert.isFalse(userRegistration.emailValidator("himanshugharat@gmail"));
+    });
+    it("returns false if email id dont have @ and domain name", function () {
+      assert.isFalse(userRegistration.emailValidator("himanshugharat"));
+    });
+  });
