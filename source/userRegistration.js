4dc863dc4d43a1a624b52d1846e1535100889625
himanshu <himanshugharat44@gmail.com>
2020-09-16 23:50:11 -0500

    REFACTORED: test case and code to check for null and undefined values

--- source/userRegistration.js
+++ source/userRegistration.js
@@ -2,7 +2,7 @@
  *Execution: default node cmd>node userRegistration.js
  *Purpose  : To validate the user registration
  *@files   : userRegistration.js
- *@overview: validate user registration 
+ *@overview: validate user registration
  *@author  : Himanshu Gharat
  *@verson  : 1.0
  *@since   : 16-09-2020
@@ -13,33 +13,36 @@ module.exports = {
    * @description: validate the name
    * @returns: true or false
    */
-  nameValidator: function (name) {
+  nameValidator: (name) => {
     let regexConst = new RegExp(/^[A-Z][A-Za-z]{2,}/);
     return regexConst.test(name);
   },
+
   /**
    * @description: validate the emailid
    * @returns: true or false
    */
-  emailValidator: function (email) {
+  emailValidator: (email) => {
     let regexConst = new RegExp(
       /^[0-9a-z]+[+_.-]?[0-9a-z]+[@][0-9a-z]+[.][a-z]{2,}[.]?[a-z]+$/
     );
     return regexConst.test(email);
   },
+
   /**
    * @description: validate the phone number
    * @returns: true or false
    */
-  mobileNoValidator: function (number) {
+  mobileNoValidator: (number) => {
     let regexConst = new RegExp(/^[0-9]{2}[ ][0-9]{10}/);
     return regexConst.test(number);
   },
+
   /**
    * @description: validate the password
    * @returns: true or false
    */
-  passwordValidator: function (password) {
+  passwordValidator: (password) => {
     let regexConst = new RegExp(
       /(?=.*[A-Z].*)(?=.*[0-9].*)([a-zA-Z0-9]{4,}[!@#$%^&*()_+][a-zA-Z0-9]{3,})/
     );
--- test/registrationTest.js
+++ test/registrationTest.js
@@ -1,86 +1,116 @@
 const assert = require("chai").assert;
 const userRegistration = require("../source/userRegistration");
 
-describe("First Name validator ", function () {
-  it("returns true if first name is valid", function () {
+describe("First Name validator", function () {
+  it("given first name if valid then should returns true", function () {
     assert.isTrue(userRegistration.nameValidator("Himanshu"));
   });
-  it("returns false if first name is not valid", function () {
+  it("given first name if not valid then should returns false", function () {
     assert.isFalse(userRegistration.nameValidator("himanshu"));
   });
-  it("returns false if first name starts from number", function () {
+  it("given first name if starts with number then should returns false", function () {
     assert.isFalse(userRegistration.nameValidator("1Himanshu"));
   });
-  it("returns false if first name starts with symbol", function () {
+  it("given first name if contains symbols then should returns false", function () {
     assert.isFalse(userRegistration.nameValidator("@Himanshu"));
+  });
+  it("given first name if null then should returns false", function () {
+    assert.isFalse(userRegistration.nameValidator(null));
+  });
+  it("given first name if undifined then should returns false", function () {
+    assert.isFalse(userRegistration.nameValidator(undefined));
   });
 });
 
 describe("Last Name validator ", function () {
-  it("returns true if last name is valid", function () {
+  it("given last name if valid then should returns true", function () {
     assert.isTrue(userRegistration.nameValidator("Gharat"));
   });
-  it("returns false if last name is not valid", function () {
+  it("given last name if not starts with uppercase then should returns false", function () {
     assert.isFalse(userRegistration.nameValidator("gharat"));
   });
-  it("returns false if last name starts from number", function () {
+  it("given last name if contains number then should returns false", function () {
     assert.isFalse(userRegistration.nameValidator("1gharat"));
   });
-  it("returns false if last name starts with symbol", function () {
+  it("given last name if contains symbol then should return false", function () {
     assert.isFalse(userRegistration.nameValidator("@Gharat"));
+  });
+  it("given last name if null then should returns false", function () {
+    assert.isFalse(userRegistration.nameValidator(null));
+  });
+  it("given last name if undifined then should returns false", function () {
+    assert.isFalse(userRegistration.nameValidator(undefined));
   });
 });
 
 describe("Email id validator ", function () {
-  it("returns true if email id is valid", function () {
+  it("given email id if valid then should returns true", function () {
     assert.isTrue(userRegistration.emailValidator("himanshugharat3@gmail.com"));
   });
-  it("returns false if email dont have @ symbol is not valid", function () {
+  it("given email id if does not contain @ then should returns false", function () {
     assert.isFalse(userRegistration.emailValidator("himanshugharat.com"));
   });
-  it("returns false if email id dont have domain name", function () {
+  it("given email id if does not contains domain name then should returns false", function () {
     assert.isFalse(userRegistration.emailValidator("himanshugharat@gmail"));
   });
-  it("returns false if email id dont have @ and domain name", function () {
+  it("given email id if does not contains domain name and @ then should returns false", function () {
     assert.isFalse(userRegistration.emailValidator("himanshugharat"));
+  });
+  it("given email id if null then should returns false", function () {
+    assert.isFalse(userRegistration.emailValidator(null));
+  });
+  it("given email id if undifined then should returns false", function () {
+    assert.isFalse(userRegistration.emailValidator(undefined));
   });
 });
 
 describe("Phone Number validator ", function () {
-  it("returns true if phone number is valid", function () {
+  it("given phone number if valid then should return true", function () {
     assert.isTrue(userRegistration.mobileNoValidator("91 8879112321"));
   });
-  it("returns false if phone number contains symbol ", function () {
+  it("given phone number if contains symbols then should return false", function () {
     assert.isFalse(userRegistration.mobileNoValidator("91@8879112321"));
   });
-  it("returns false if phone number dont have space between code and number", function () {
+  it("given phone number if does not contains space then should return false", function () {
     assert.isFalse(userRegistration.mobileNoValidator("919619326117"));
   });
-  it("returns false if phone number has letters or symbols in it", function () {
+  it("given phone number if contains letters then should return false", function () {
     assert.isFalse(userRegistration.mobileNoValidator("12345556Q22ee"));
   });
-  it("returns false if phone number is more than 10 digit", function () {
+  it("given phone number if contains phone number more than 10 digits then should return false", function () {
     assert.isFalse(userRegistration.mobileNoValidator("911 887911202111"));
   });
-  it("returns false if phone number is less than 10 digit", function () {
+  it("given phone number if contains phone number less than 10 digits then should return false", function () {
     assert.isFalse(userRegistration.mobileNoValidator("91 887911"));
+  });
+  it("given phone number if null then should returns false", function () {
+    assert.isFalse(userRegistration.mobileNoValidator(null));
+  });
+  it("given phone number name if undifined then should returns false", function () {
+    assert.isFalse(userRegistration.mobileNoValidator(undefined));
   });
 });
 
 describe("Password validator ", function () {
-  it("returns true if password is valid", function () {
+  it("given password if valid then should return true", function () {
     assert.isTrue(userRegistration.passwordValidator("Qwsdefq@123"));
   });
-  it("returns false if password is less than 8 chars", function () {
+  it("given password if less than 8 char then should return false", function () {
     assert.isFalse(userRegistration.passwordValidator("hehdc"));
   });
-  it("returns false if password contains no upper case but 8 char long ", function () {
+  it("given password if no uppercase but 8 char long then should return false ", function () {
     assert.isFalse(userRegistration.passwordValidator("qwertyuio"));
   });
-  it("returns false if password contains no digit but has upper case and 8 char long ", function () {
+  it("given password if no digit but has uppercase and 8 char long then should return false ", function () {
     assert.isFalse(userRegistration.passwordValidator("Qwertyuio"));
   });
-  it("returns false if password contains no symbol but has digit, upper case and 8 char long ", function () {
+  it("given password if no symbol but has digit, uppercase and 8 char long then should return false ", function () {
     assert.isFalse(userRegistration.passwordValidator("Qwertyuio12"));
+  });
+  it("given password if null then should returns false", function () {
+    assert.isFalse(userRegistration.passwordValidator(null));
+  });
+  it("given password if undifined then should returns false", function () {
+    assert.isFalse(userRegistration.passwordValidator(undefined));
   });
 });
